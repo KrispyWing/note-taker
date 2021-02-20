a4f192375542a5911f297f21ab7e885ad7a060d0
Kris Pennimpede <kpennimpede@hotmail.com>
2021-02-20 17:59:50 -0500

    proper routing to deliver HTML pages added to server.js

--- server.js
+++ server.js
@@ -15,6 +15,17 @@ app.use(express.urlencoded({ extended: true }));
 //parse incoming JSON data
 app.use(express.json());
 
+app.use(express.static('public'));
+
+//routes for the HTML Pages
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}`);
 });
